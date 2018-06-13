385ff711334e9efc6d9136792abd5e2631d737ac
bassel <edbavv@gmail.com>
2018-06-13 14:20:36 +0300

    Fetch port from heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -59,6 +60,6 @@ app.get('/bad', (req, res) => {
 
 //bind the app to a port on our machine
 //second argument of listen is optional
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
