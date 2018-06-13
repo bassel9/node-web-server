44c5c8ebf3b2316d765d5a8a38931168e9f9b346
bassel <edbavv@gmail.com>
2018-06-13 11:29:03 +0300

    Append logs to a file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+/*jshint esversion: 6 */
+
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
@@ -10,8 +13,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
   var now = new Date().toString();
-
-  console.log(`${now}: ${req.method} ${req.url}`);
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err)
+      console.log(err);
+  });
   //any async task will be executed until
   //you cal next, if you don't call it,
   //the page will never load since the routes
@@ -23,24 +30,24 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text)=> {
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
-})
+});
 
-app.get('/', (request, response)=> {
+app.get('/', (request, response) => {
   response.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website',
-  })
+  });
 });
 
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
   });
 });
 
-app.get('/bad', (req, res)=> {
+app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle this request'
   });
